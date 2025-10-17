8503c32a66bd6a5046620fa044dd5149f1b4c2d1
EvgenSan <evgnesan@yandex.ru>
2025-10-17 12:59:55 +0530

    refactor(extended-1.1): converted the HTML page to semantic tags

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
+    event.preventDefault();
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
